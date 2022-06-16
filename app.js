8f8401566214178f5f5c15e10ff0cd68e0eeb617
RahmN <rahmanxrf@gmail.com>
2022-06-16 23:20:01 +0430

    install morgan & connet to MongoDB & use boostrap and fa

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const path = require('path');
 
 const express = require('express');
 const dotEnv = require('dotenv');
+const morgan = require('morgan');
 
+const connectDB = require('./config/db');
 const indexRoutes = require('./routes/index');
 
 //* Load Config
 dotEnv.config({path: "./config/config.env"});
 
+//*DataBase connection
+connectDB();
+
 const app = express();
+
+//*Logging
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan("dev"));
+}
 
 //* view Engine
 app.set('view engine', 'ejs');
@@ -16,6 +26,8 @@ app.set('views', 'views');
 
 //* Static Folders
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, process.env.BOOTSTRAP )));
+// app.use(express.static(path.join(__dirname, process.env.FONTAWESOME)));
 
 //* Routes
 app.use(indexRoutes);

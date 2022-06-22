fddd1a00f1be9419069d5c859ebd6eec3870cacd
RahmN <rahmanxrf@gmail.com>
2022-06-22 10:46:26 +0430

    validate users information with Yup

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,16 @@
 const {
     Router
 } = require("express");
+const yup = require('yup');
 
 const router = new Router();
+
+const schema =  yup.object().shape({
+    fullname: yup.string().required("نام الزامی است").min(4).max(255),
+    email: yup.string().email().required("ایمیل معتبر نیست"),
+    password: yup.string().min(4).max(255),
+    confirmPassword: yup.string().required().oneOf([yup.ref("password"),null])
+});
 
 //* @desc weblog login page
 //* routes GET /users/loginPage
@@ -25,8 +33,13 @@ router.get('/signin', (req, res) => {
 //* @desc weblog handle page
 //* routes POST /users/handle Page
 router.post('/signin', (req, res) => {
-    console.log(req.body);
-    res.send("weblog");
+    schema.validate(req.body).then((result) =>{
+        console.log(result);
+        res.send("All Good");
+    }).catch((err) =>{
+        console.log(err);
+        res.send("Error" , {errors: err.errors});
+    })
 });
 
 module.exports = router

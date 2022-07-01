426e275f54ef06dba34a380a1e8d73de56183ac2
RahmN <rahmanxrf@gmail.com>
2022-07-01 21:54:48 +0430

    flash-massaging for sucsessfully register user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,11 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.login = (req, res) => {
     res.render('login', {
         pageTitle: "ورود به بخش مدیریت",
-        path: "/login"
+        path: "/login",message : req.flash("success_msg")
     });
 }
 
@@ -18,21 +20,52 @@ exports.createUser = async (req, res) => {
     const errors = [];
     try {
         await User.userValidation(req.body);
-        const { fullname ,email , password } = req.body;
+        const {
+            fullname,
+            email,
+            password
+        } = req.body;
 
-        const user = await User.findOne({email})
-        if(user){
-            errors.push({message: "کاربری با ایمیل وارد شده قبلا ثبت نام شده است"});
+        const user = await User.findOne({
+            email
+        })
+        if (user) {
+            errors.push({
+                message: "کاربری با ایمیل وارد شده قبلا ثبت نام شده است"
+            });
             return res.render('signin', {
                 pageTitle: "ثبت نام",
                 path: "/signin",
                 errors,
             });
-        }else
-
-        await User.create(req.body); //!solution 1
+        }
+        //*hash password
+        //TODO: solution shortly for hashing password
+        const hash = await bcrypt.hash(password, 10)
+        await User.create({
+            fullname,
+            email,
+            password: hash
+        })
+        req.flash("success_msg" , "ٍثبت نام موفقیت امیز بود")
         res.redirect("/users/login");
-        //    const { fullname ,email , password } = req.body; //!solution 2
+
+        //TODO: solution normaly for hashing password
+        // bcrypt.genSalt(10, (err, salt) => {
+        //     if (err) throw err;
+        //     bcrypt.hash(password, salt, async (err, hash) => {
+        //         if (err) throw err;
+
+        //         await User.create({
+        //             fullname ,
+        //             email , 
+        //             password : hash
+        //         }); //!solution 1 register to database
+        //         res.redirect("/users/login");
+        //     })
+        // })
+        //!___________________________________________________________
+        //    const { fullname ,email , password } = req.body; //!solution 2 register to database
         //    const user = new User({
         //     fullname ,
         //     email , 
